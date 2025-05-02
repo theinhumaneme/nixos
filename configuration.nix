53585c5e779b22da80c55e956663983265365c72
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-05-03 00:06:55 +0530

    [gnome] remove unused packages

--- configuration.nix
+++ configuration.nix
@@ -51,6 +51,23 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
+  environment.gnome.excludePackages = with pkgs; [
+    epiphany # web browser
+    simple-scan # document scanner
+    totem # video player
+    yelp # help viewer
+    evince # document viewer
+    geary # email client
+    gnome-characters
+    gnome-contacts
+    gnome-logs
+    gnome-maps
+    gnome-music
+    gnome-photos
+    gnome-weather
+    gnome-connections
+    gnome-tour
+  ];
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
