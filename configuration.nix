5de8ba35563011b4fec00a4e365fabb95d6bf229
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-05-02 23:32:43 +0530

    [system] add git

--- configuration.nix
+++ configuration.nix
@@ -106,6 +106,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    git
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
