f7ea597be774fb74cdf5e84566f02a0fc195e54e
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-05-06 23:10:38 +0530

    [user] make plasma wayland only

---
+++ modules/plasma.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+{
+
+  services = {
+    displayManager = {
+      sddm.wayland.enable = true;
+      sddm.enable = true;
+    };
+    desktopManager.plasma6.enable = true;
+  };
+
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    oxygen
+    elisa
+  ];
+}
