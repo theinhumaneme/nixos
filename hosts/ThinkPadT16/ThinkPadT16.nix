efc4c310ee3f8fc96bdec04d9b6fd9d3867f6d13
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-05-07 01:17:23 +0530

    [system] add cloudflare IPv6 DNS resolvers

--- hosts/ThinkPadT16/ThinkPadT16.nix
+++ hosts/ThinkPadT16/ThinkPadT16.nix
@@ -96,6 +96,8 @@ in
   networking.nameservers = [
     "1.1.1.1#one.one.one.one"
     "1.0.0.1#one.one.one.one"
+    "2606:4700:4700::1111#one.one.one.one"
+    "2606:4700:4700::1001#one.one.one.one"
   ];
 
   # Enable automatic login for the user.
