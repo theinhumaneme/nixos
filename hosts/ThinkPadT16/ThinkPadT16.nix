35aaf750be1cf65e2cc45629a049d034a6980c86
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-05-15 21:58:48 +0530

    [system] add fwupd service

--- hosts/ThinkPadT16/ThinkPadT16.nix
+++ hosts/ThinkPadT16/ThinkPadT16.nix
@@ -159,8 +159,9 @@ in
     mesa-demos
     vulkan-tools
     busybox
-
+    fwupd
   ];
+  services.fwupd.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
