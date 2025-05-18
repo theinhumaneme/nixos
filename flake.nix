500ea6fffd2d486f93f242c9586980658ade8929
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-05-18 16:09:22 +0530

    [nix] switch to 25.05 channel

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-24.11";
+      url = "github:NixOS/nixpkgs/nixos-25.05";
     };
     nixpkgsUnstable = {
       url = "github:NixOS/nixpkgs/nixos-unstable";
