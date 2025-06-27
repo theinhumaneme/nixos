dec5f538ba23278c0e58867a3a2fac9eda4f4740
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-06-28 01:19:58 +0530

    [auto-cpufreq] update configuration

--- modules/auto-cpufreq.nix
+++ modules/auto-cpufreq.nix
@@ -15,10 +15,10 @@
   services.auto-cpufreq.enable = true;
   services.auto-cpufreq.settings = {
     charger = {
-      governor = "performance";
-      turbo = "auto";
-      energy_performance_preference = "performance";
-      platform_profile = "performance";
+      governor = "powersave";
+      turbo = "never";
+      energy_performance_preference = "power";
+      platform_profile = "low-power";
     };
 
     battery = {
