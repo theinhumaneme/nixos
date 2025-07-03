66b4500baa105cb0bb6f80a23a62909ce803c88f
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-07-03 20:24:56 +0530

    [system] reduce swappiness and dirty_bytes configuration

--- modules/sysctl-config.nix
+++ modules/sysctl-config.nix
@@ -5,7 +5,7 @@
   boot.kernelModules = [ "tcp_bbr" ];
   boot.kernel.sysctl = {
     # Memory management
-    "vm.swappiness" = 100;
+    "vm.swappiness" = 10;
 
     # The value controls the tendency of the kernel to reclaim the memory which is used for caching of directory and inode objects (VFS cache).
     # Lowering it from the default value of 100 makes the kernel less inclined to reclaim VFS cache (do not set it to 0, this may produce out-of-memory conditions)
@@ -13,7 +13,7 @@
 
     # Contains, as bytes, the number of pages at which a process which is
     # generating disk writes will itself start writing out dirty data.
-    "vm.dirty_bytes" = 268435456;
+    "vm.dirty_bytes" = 134217728;
 
     # page-cluster controls the number of pages up to which consecutive pages are read in from swap in a single attempt.
     # This is the swap counterpart to page cache readahead. The mentioned consecutivity is not in terms of virtual/physical addresses,
@@ -23,7 +23,7 @@
 
     # Contains, as bytes, the number of pages at which the background kernel
     # flusher threads will start writing out dirty data.
-    "vm.dirty_background_bytes" = 67108864;
+    "vm.dirty_background_bytes" = 33554432;
 
     # The kernel flusher threads will periodically wake up and write old data out to disk.  This
     # tunable expresses the interval between those wakeups, in 100'ths of a second (Default is 500).
