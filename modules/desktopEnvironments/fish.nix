a1416c47df764d73e659e37c6ac862a79fa83010
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-07-03 22:00:58 +0530

    [user] enable starship prompt

--- modules/desktopEnvironments/fish.nix
+++ modules/desktopEnvironments/fish.nix
@@ -4,12 +4,22 @@
   ...
 }:
 {
-  programs.fish = {
-    enable = true;
-  };
   users.users."${userName}" = {
     shell = pkgsUnstable.fish;
     packages = with pkgsUnstable; [
+      starship
     ];
+  };
+
+  programs = {
+    fish = {
+      enable = true;
+      interactiveShellInit = ''
+        starship init fish | source
+      '';
+    };
+    starship = {
+      enable = true;
+    };
   };
 }
