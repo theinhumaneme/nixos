6d272c4fc35ac1719fc8e6ac55840b9af241aecb
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-07-17 22:49:03 +0530

    pkgs: add proton-cachyos-v3 to steam

--- hosts/ThinkPadT16/user.nix
+++ hosts/ThinkPadT16/user.nix
@@ -60,7 +60,10 @@
     steam = {
       enable = true;
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-      extraCompatPackages = with pkgs; [ proton-ge-custom ];
+      extraCompatPackages = with pkgs; [
+        proton-ge-custom
+        proton-cachyos_x86_64_v3
+      ];
     };
   };
   services = {
