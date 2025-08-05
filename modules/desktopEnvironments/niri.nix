ffa8cad123a3d1d58003c1bff5a27d1b8a177208
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-08-05 18:29:58 +0530

    pkgs: move xwayland to unstable

--- modules/desktopEnvironments/niri.nix
+++ modules/desktopEnvironments/niri.nix
@@ -23,13 +23,13 @@
     # Apps
     with pkgs;
     [
-      xwayland-satellite # XWayland Integration
       xdg-desktop-portal-gtk
       xdg-desktop-portal-gnome # required for screen-casting
       gnome-keyring # for secret management
       polkit_gnome # polkit agent
     ]
     ++ (with pkgsUnstable; [
+      xwayland-satellite # XWayland Integration
       mako # notification daemon
       alacritty # terminal
       walker
