330aa1108a0eb4d71b4976ce570afe226420a017
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-08-07 21:38:42 +0530

    neovim: configure treesitter

--- modules/devStuff/neovim/plugins/treesitter.nix
+++ modules/devStuff/neovim/plugins/treesitter.nix
@@ -1,46 +1,79 @@
+{ pkgs, ... }:
 {
   programs.nixvim = {
-    # Useful plugin to show you pending keybinds.
-    # https://nix-community.github.io/nixvim/plugins/which-key/index.html
-    plugins.which-key = {
+    # Highlight, edit, and navigate code
+    # https://nix-community.github.io/nixvim/plugins/treesitter/index.html
+    plugins.treesitter = {
       enable = true;
 
-      # Document existing key chains
+      # Installing tree-sitter grammars from Nixpkgs (recommended)
+      # https://nix-community.github.io/nixvim/plugins/treesitter/index.html#installing-tree-sitter-grammars-from-nixpkgs
+      # grammarPackages = pkgs.vimPlugins.nvim-treesitter.passthru.allGrammars;
+      grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+        bash
+        ssh_config
+        tmux
+        nix
+        vim
+        vimdoc
+        lua
+        luadoc
+        csv
+        diff
+        editorconfig
+        git_config
+        git_rebase
+        gitattributes
+        gitcommit
+        gitignore
+        ini
+        markdown
+        markdown_inline
+        regex
+        xml
+        yaml
+        rust
+        toml
+        css
+        html
+        javascript
+        json
+        sql
+        scss
+        tsx
+        typescript
+        nginx
+      ];
+
       settings = {
-        spec = [
-          {
-            __unkeyed-1 = "<leader>c";
-            group = "[C]ode";
-          }
-          {
-            __unkeyed-1 = "<leader>d";
-            group = "[D]ocument";
-          }
-          {
-            __unkeyed-1 = "<leader>r";
-            group = "[R]ename";
-          }
-          {
-            __unkeyed-1 = "<leader>s";
-            group = "[S]earch";
-          }
-          {
-            __unkeyed-1 = "<leader>w";
-            group = "[W]orkspace";
-          }
-          {
-            __unkeyed-1 = "<leader>t";
-            group = "[T]oggle";
-          }
-          {
-            __unkeyed-1 = "<leader>h";
-            group = "Git [H]unk";
-            mode = [
-              "n"
-              "v"
-            ];
-          }
+        # Installing tree-sitter grammars from nvim-treesitter
+        # (can be combined with grammarPackages from Nixpkgs)
+        # https://nix-community.github.io/nixvim/plugins/treesitter/index.html#installing-tree-sitter-grammars-from-nvim-treesitter
+        ensureInstalled = [
         ];
+
+        highlight = {
+          enable = true;
+
+          # Some languages depend on vim's regex highlighting system for indent rules.
+          additional_vim_regex_highlighting = [
+            "ruby"
+          ];
+        };
+
+        indent = {
+          enable = true;
+          disable = [
+            "ruby"
+          ];
+        };
+
+        # There are additional nvim-treesitter modules that you can use to interact
+        # with nvim-treesitter. You should go explore a few and see what interests you:
+        #
+        #    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+        #    - Show your current context: https://nix-community.github.io/nixvim/plugins/treesitter-context/index.html
+        #    - Treesitter + textobjects: https://nix-community.github.io/nixvim/plugins/treesitter-textobjects/index.html
       };
     };
   };
