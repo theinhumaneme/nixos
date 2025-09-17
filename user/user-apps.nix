6b4bf0d34e8a400c1769195d85beda06d2231680
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-09-17 15:22:15 -0400

    user: add obs-studio

--- user/user-apps.nix
+++ user/user-apps.nix
@@ -28,5 +28,16 @@
         slack
       ]);
   };
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-vaapi # optional AMD hardware acceleration
+      obs-gstreamer
+      obs-vkcapture
+    ];
+  };
 
 }
