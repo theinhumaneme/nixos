25358e64b97cf6bbc2e931857eee49cab4b875c0
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-10-10 10:41:41 -0400

    SER7: disable autologin

--- hosts/SER7/SER7.nix
+++ hosts/SER7/SER7.nix
@@ -93,7 +93,7 @@ in
   services.fprintd.enable = false;
 
   # Enable automatic login for the user.
-  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.enable = false;
   services.displayManager.autoLogin.user = userName;
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
