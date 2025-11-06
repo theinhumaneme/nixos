b6428057e97089d8c64dbabd205bb531bc3f2913
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-11-05 20:24:06 -0500

    common-dev: add hugo

--- modules/devStuff/common.nix
+++ modules/devStuff/common.nix
@@ -6,7 +6,7 @@
   ...
 }:
 {
-  imports = [./jetbrains.nix];
+  imports = [ ./jetbrains.nix ];
   users.users."${userName}".packages = (
     lib.optionals enableDevTools (
       with pkgsUnstable;
@@ -19,7 +19,7 @@
         lazygit
         ripgrep
         tmux
-
+        hugo
       ]
     )
   );
