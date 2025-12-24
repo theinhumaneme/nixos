aabfc11f87829b415aaa25260c6d46f89853aac4
Kalyan Mudumby <kalyanmudumby@MBP14.local>
2025-12-24 02:07:00 -0600

    abstract nix to separate file

--- hosts/common/common.nix
+++ hosts/common/common.nix
@@ -2,38 +2,13 @@
 {
   imports = [
     ./fish.nix
+    ./nix.nix
     ../../user/desktop-apps.nix
     ../../user/user-apps.nix
     ./../../user/user.nix
     ../../modules/browser.nix
 
   ];
-  nix = {
-    settings = {
-      substituters = [
-        "https://cache.nixos.org/"
-        "https://install.determinate.systems"
-      ];
-      trusted-public-keys = [
-        "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
-        "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
-      ];
-      auto-optimise-store = true;
-      experimental-features = [
-        "nix-command"
-        "flakes"
-        "parallel-eval"
-      ];
-      lazy-trees = true;
-      eval-cores = 0;
-    };
-
-    gc = {
-      automatic = true;
-      dates = "daily";
-      options = "--delete-older-than 7d";
-    };
-  };
 
   # Select internationalisation properties.
   i18n = {
