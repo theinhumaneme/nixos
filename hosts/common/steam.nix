a0541b9061382c132433dabb5b556b7db6eb2f0b
Kalyan Mudumby <theinhumaneme@gmail.com>
2026-01-09 18:22:28 -0500

    remove sched stuff from steam.nix

--- hosts/common/steam.nix
+++ hosts/common/steam.nix
@@ -6,12 +6,4 @@
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     };
   };
-  services = {
-    scx = {
-      package = pkgs.scx.full;
-      enable = true;
-      scheduler = "scx_lavd";
-      extraArgs = [ "--autopower" ];
-    };
-  };
 }
